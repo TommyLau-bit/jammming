81c6952beaffc6e6a4600c4a01bf17cd6db24dac
TommyLau-bit <146777147+TommyLau-bit@users.noreply.github.com>
2024-12-15 08:24:33 +0800

    Implement Removing Songs From a Custom Playlist

--- src/components/Track/Track.js
+++ src/components/Track/Track.js
@@ -2,20 +2,29 @@
 import React from 'react';
 import './Track.css';
 
-function Track({name, artist, album, onAdd, isRemoval}) {
+function Track({id, name, artist, album, onAdd, onRemove, isRemoval}) {
     const handleAdd = () => {
         if (onAdd) {
           onAdd({ id, name, artist, album });
         }
       };
-      
+      const handleRemove = () => {
+        if (onRemove) {
+          onRemove(id);
+        }
+      };
+
   return (
     <div className="Track">
       <div className="Track-information">
         <h3>{name}</h3>
         <p>{artist} | {album}</p>
       </div>
-      {!isRemoval && <button onClick={handleAdd}>+</button>}
+      {isRemoval ? (
+        <button onClick={handleRemove}>-</button> // Remove button
+      ) : (
+        <button onClick={handleAdd}>+</button> // Add button
+      )}
     </div>
   );
 }
--- src/components/Tracklist/Tracklist.js
+++ src/components/Tracklist/Tracklist.js
@@ -8,6 +8,7 @@ function Tracklist({tracks, onAdd, isRemoval}) {
     <div className="Tracklist">
         {tracks.map(track => (
       <Track key={track.id} 
+             id={track.id}
              name={track.name} 
              artist={track.artist} 
              album={track.album} 
